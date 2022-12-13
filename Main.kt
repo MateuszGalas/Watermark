9feaae4f1b95450660fe81b53c517d057c2559f9
Mateusz Galaś <mateuszgalas94@gmail.com>
2022-12-13 21:48:48 +0100

    In the middle of adding single and grid methods.

--- Main.kt
+++ Main.kt
@@ -6,12 +6,14 @@ import java.awt.image.BufferedImage
 import java.io.File
 import java.lang.Exception
 import javax.imageio.ImageIO
+import kotlin.math.sin
 
 fun main() {
     try {
         var alphaChannel = false
         var transparency = false
         var transparencyColor = Color(0, 0, 0)
+        var single = false
 
         println("Input the image filename:")
         val image = File(readln())
@@ -23,8 +25,8 @@ fun main() {
 
         val myImage = ImageIO.read(image)
         val myWatermark = ImageIO.read(watermark)
-        if (myImage.width != myWatermark.width || myImage.height != myWatermark.height) {
-            return println("The image and watermark dimensions are different.")
+        if (myImage.width < myWatermark.width || myImage.height < myWatermark.height) {
+            return println("The watermark's dimensions are larger.")
         }
 
         if (myWatermark.transparency == Transparency.TRANSLUCENT) {
@@ -60,6 +62,25 @@ fun main() {
             }
         }.let { if (it in 0..100) it else return println("The transparency percentage is out of range.") }
 
+        println("Choose the position method (single, grid):")
+        single = readln().let {
+            if (it.equals("single", true)) {
+                true
+            } else if (it.equals("grid", true)) {
+                false
+            } else {
+                return println("The position method input is invalid.")
+            }
+        }
+
+        /*if (single) {
+            println("Input the watermark position " +
+                    "([x 0-${myImage.width - myWatermark.width}] [y 0-${myImage.height - myWatermark.height}):")
+
+        } else {
+
+        }*/
+
         println("Input the output image filename (jpg or png extension):")
         val output = File(readln().let {
             if (it.contains(".png") || it.contains(".jpg")) {
@@ -70,11 +91,18 @@ fun main() {
         }).apply { if (!this.exists()) this.createNewFile() }
 
         val myOutput = BufferedImage(myImage.width, myImage.height, BufferedImage.TYPE_INT_RGB)
+        var x2 = 0
+        var y2 = 0
+        var singleX = 0
+        var singleY = 0
 
         for (x in 0 until myImage.width) {
             for (y in 0 until myImage.height) {
+                if (x2 >= myWatermark.width) x2 = 0
+                if (y2 >= myWatermark.height) y2 = 0
+
                 val i = Color(myImage.getRGB(x, y))
-                val w = Color(myWatermark.getRGB(x, y), alphaChannel)
+                val w = Color(myWatermark.getRGB(x2, y2), alphaChannel)
 
                 val color = Color(
                     (weight * w.red + (100 - weight) * i.red) / 100,
@@ -84,6 +112,8 @@ fun main() {
 
                 if (alphaChannel) {
                     if (w.alpha == 0) {
+                        myOutput.setRGB(x, y, Color(myImage.getRGB(x, y)).rgb)
+                    } else if (single && (singleX >= myWatermark.width || singleY >= myWatermark.height)) {
                         myOutput.setRGB(x, y, Color(myImage.getRGB(x, y)).rgb)
                     } else {
                         myOutput.setRGB(x, y, color.rgb)
@@ -99,7 +129,12 @@ fun main() {
                         myOutput.setRGB(x, y, color.rgb)
                     }
                 }
+                if (singleY == myImage.height) singleY = 0
+                singleY++
+                y2++
             }
+            singleX++
+            x2++
         }
 
         ImageIO.write(myOutput, output.name.substring(output.name.lastIndex - 2), output)
